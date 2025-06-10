dd90f74f9aac9d92b079a0a8133f99d0fdc4ca4f
c0a24040ad <c0a24040ad@edu.teu.ac.jp>
2025-06-10 15:36:37 +0900

    練習５こうかとん右から左へ

--- pygame_image.py
+++ pygame_image.py
@@ -17,11 +17,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #スクリーンに別のSurfaceを貼り付ける
+        screen.blit(bg_img, [-tmr, 0]) #スクリーンに別のSurfaceを貼り付ける
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(1)
+        clock.tick(10)
 
 
 if __name__ == "__main__":
