b989c88340f3b2bd41cc74359cfdd2681fac7124
c0a24040ad <c0a24040ad@edu.teu.ac.jp>
2025-06-10 15:58:31 +0900

    練習９残像処理

--- pygame_image.py
+++ pygame_image.py
@@ -18,9 +18,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])   #$スクリーンに別のSurfaceを貼り付ける
-        screen.blit(bg_img2, [-tmr+1600, 0])
-        #screen.blit(bg_img, [-tmr+3200, 0])
+        x=tmr%3200
+        screen.blit(bg_img, [-x, 0])   #$スクリーンに別のSurfaceを貼り付ける
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
